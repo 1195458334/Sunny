e2935e59e43f5a4503a18f111d785be219c4e493
luojun <1195458334@qq.com>
2019-07-26 17:37:37 +0800

    初始化

---
+++ sunny-starter-cache/src/main/java/com/lyyzoo/cache/SunnyStarterCacheApplication.java
@@ -0,0 +1,13 @@
+package com.lyyzoo.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunnyStarterCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunnyStarterCacheApplication.class, args);
+    }
+
+}
---
+++ sunny-starter-core/src/main/java/com/lyyzoo/core/SunnyStarterCoreApplication.java
@@ -0,0 +1,14 @@
+package com.lyyzoo.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunnyStarterCoreApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(SunnyStarterCoreApplication.class, args);
+    }
+
+}
